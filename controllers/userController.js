26941f34df54540682818f55173671d05430a95a
Davyd Maksimovich <dav.maks.dev@gmail.com>
2025-09-21 14:42:51 +0300

    feat (user): create userModel, userController, userRoutes and link it with server

---
+++ controllers/userController.js
@@ -0,0 +1,55 @@
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+
+//create new user
+exports.createUser = async (req, res) => {
+  try {
+    const { username, password, role } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).json({ error: "Username and password required" });
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // створюємо юзера
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+      role: role || "user",
+    });
+
+    await newUser.save();
+    res
+      .status(201)
+      .json({ message: "User created successfully", user: newUser });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({}, "-password"); // without passwords
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id, "-password"); //without a password
+    if (!user) return res.status(404).json({ error: "User not found" });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = { createUser, getAllUsers, getUserById };
--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello, Military Tests!");
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+/*
+mongoose.connect("mongodb://127.0.0.1:27017/military-tests", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("MongoDB connected");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+.catch(err => console.error("MongoDB connection error:", err));
+*/
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
